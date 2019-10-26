081b341d1126ecd52940da63e6638e10fa5dfc92
TheFedExpress <pgoodridge2007@gmail.com>
2019-10-26 14:39:00 -0400

    setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 26 14:37:15 2019
+
+@author: pgood
+"""
+
+from setuptools import setup
+
+setup(name='HelperFuncs', version='0.1', scripts=['HelperFuncs.py'])
